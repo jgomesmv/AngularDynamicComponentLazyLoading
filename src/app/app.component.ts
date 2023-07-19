76773b9722bdfd104d3f6d3e3e285f4fa775f1da
José Gomes <jose.gomes@purefacts.com>
2023-07-19 16:20:07 +0100

    initial solution commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component, Type, ViewChild, ViewContainerRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+import { BehaviorSubject, Observable, switchMap, tap } from 'rxjs';
+
+import { ComponentResolverService } from './services/component-resolver.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  @ViewChild('componentContainer', { read: ViewContainerRef, static: true })
+  componentContainer: ViewContainerRef | undefined;
+
+  currentVehicle$ = new BehaviorSubject<'car' | 'motorbike'>('car');
+  component$ = this.getComponent();
+
+  constructor(private componentResolverService: ComponentResolverService) {}
+
+  onVehicleToogle(): void {
+    const nextVehicle =
+      this.currentVehicle$.getValue() === 'car' ? 'motorbike' : 'car';
+    this.currentVehicle$.next(nextVehicle);
+  }
+
+  getComponent(): Observable<Type<unknown>> {
+    return this.currentVehicle$.pipe(
+      switchMap((currentVehicle) => {
+        return this.componentResolverService.getComponentType(currentVehicle);
+      }),
+      tap((componentType) => {
+        const viewContainerRef = this.componentContainer;
+        viewContainerRef?.clear();
+
+        const componentRef =
+          viewContainerRef?.createComponent<unknown>(componentType);
+        componentRef?.setInput('title', this.currentVehicle$.getValue());
+      })
+    );
+  }
+}
